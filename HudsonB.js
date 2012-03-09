2a1fc1684fb9a75a5ec9caaf4e683813fcea8361
djhb317 <hbinupt@yahoo.com>
2012-03-08 23:20:25 -0500

    Update HudsonB.js

--- HudsonB.js
+++ HudsonB.js
@@ -1,14 +1,20 @@
 alert("JavaScript works!");
 
 //vehicle type
-var vehicleModel = ["mercedes",
+var vehicleNames = ["mercedes",
     "ferrari",
-    "maserati"
+    "maserati"]
 ;
-
-
+var [vehicle (0) = 190000, 
+    vehicle (1) = 82000,
+    vehicle(2) = 42000]
+;
+function()
+var vehicleMiles = v ()
+;
+return{}
+//date funtion
+function getDate 
 // miles function
-var vehicleMiles 
-    vehicele1
+var vehicleMiles = []
     
-Console.log("buy now!")
