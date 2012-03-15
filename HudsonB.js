faca59f4185d250136a3e2c2d91f88106f8ed987
djhb317 <hbinupt@yahoo.com>
2012-03-15 14:47:52 -0300

    Update HudsonB.js

--- HudsonB.js
+++ HudsonB.js
@@ -1,20 +1,32 @@
-alert("JavaScript works!");
+alert("Let's Buy A Super Car!");
 
-//vehicle type
-var vehicleNames = ["mercedes",
-    "ferrari",
-    "maserati"]
+//say funcion
+var say = function(message) {console.log(message)}
 ;
-var [vehicle1 = 190000, 
-    vehicle2 = 82000,
-    vehicle3 = 42000]
-;
-function()
-var vehicleMiles = v ()
-;
-return{}
-//date funtion
-function getDate 
+
+var car=function(carName){
+    if (carName === ["Mercedes",
+    "Ferrari",
+    "Maserati"]){
+        say("nice car");
+        return true; 
+    }else{
+        say("don't buy");
+        return false; 
+    };    
+};
 // miles function
-var vehicleMiles = []
-    
+
+var milesPerCar = [15000, 18000, 12000]
+;
+for (var milage = 0; milage <= milesPerCar.length; milage+=1000){
+    var currentCarMilage = milesPerCar[milage],
+    milesPerCar = milage}
+;
+
+if (milesPerCar <= 16000){
+    say ("buy now!")}  
+    else{
+    say("Don't Buy")
+};
+
